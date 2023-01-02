5a449037ba73d4b4f74ebafd4bd06034134852ad
nyoongoon <nyoongoon@naver.com>
2023-01-02 22:39:22 +0900

    deleteTodo

--- src/main/java/com/example/spring_react/controller/TodoController.java
+++ src/main/java/com/example/spring_react/controller/TodoController.java
@@ -79,4 +79,28 @@ public class TodoController {
         // 6) Response DTO를 리턴한다.
         return ResponseEntity.ok().body(response);
     }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto){
+        try{
+            String temporaryUserId = "temporary-user";
+            // 1) TodoEntity로 변환한다.
+            TodoEntity entity = TodoDTO.toEntity(dto);
+            // 2) 임시 유저 아이디를 설정해준다.
+            entity.setUserId(temporaryUserId);
+            // 3) 서비스를 이용해 entity를 삭제한다.
+            List<TodoEntity> entities = service.delete(entity);
+            // 4) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+            // 5) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+            // 6) ResponseDTO를 리턴한다.
+            return ResponseEntity.ok().body(response);
+        }catch (Exception e){
+            // 8) 혹시 예외가 나눈 경우 dto 대신 error에 메시지를 넣어 리턴.
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
 }
